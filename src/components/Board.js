bf9931ebb452f76e132f60d92946e1aa777c824c
Matthew <matthew9655@hotmail.com>
2022-11-15 17:54:00 -0500

    update files to remove fields

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,44 +10,39 @@ function Square(props) {
   }
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return (
-          <Square
-            value={i}
-            onClick={() => this.props.onClick(i)}
-          />
-        );
-    }
+    // TODO: create and implement the function renderSquare
+    // Hint: components can be used as tags
+    // Hint: Look at the props of Square, what do we need to pass in?
 
     render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare("1")}
-                    {this.renderSquare("2")}
-                    {this.renderSquare("3")}
-                    {this.renderSquare("+")}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare("4")}
-                    {this.renderSquare("5")}
-                    {this.renderSquare("6")}
-                    {this.renderSquare("-")}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare("7")}
-                    {this.renderSquare("8")}
-                    {this.renderSquare("9")}
-                    {this.renderSquare("*")}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare("AC")}
-                    {this.renderSquare("0")}
-                    {this.renderSquare("=")}
-                    {this.renderSquare("/")}
-                </div>
-            </div>
-        );
+      return (
+          <div>
+              <div className="board-row">
+                  {this.renderSquare("1")}
+                  {this.renderSquare("2")}
+                  {this.renderSquare("3")}
+                  {this.renderSquare("+")}
+              </div>
+              <div className="board-row">
+                  {this.renderSquare("4")}
+                  {this.renderSquare("5")}
+                  {this.renderSquare("6")}
+                  {this.renderSquare("-")}
+              </div>
+              <div className="board-row">
+                  {this.renderSquare("7")}
+                  {this.renderSquare("8")}
+                  {this.renderSquare("9")}
+                  {this.renderSquare("*")}
+              </div>
+              <div className="board-row">
+                  {this.renderSquare("AC")}
+                  {this.renderSquare("0")}
+                  {this.renderSquare("=")}
+                  {this.renderSquare("/")}
+              </div>
+          </div>
+      );
     }
   }
 
